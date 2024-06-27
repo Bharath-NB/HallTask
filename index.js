6e037ccbbb1c68f4b738c6072365f2272eee10d9
Bharath-NB <Bharath.s8499@gmail.com>
2024-06-27 19:21:09 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// index.js
+
+require('dotenv').config();
+
+const express = require('express');
 const mongoose = require('mongoose');
 const { MONGODB_URI, PORT } = require('./utils/config');
 const app = require('./app');
@@ -7,10 +12,12 @@ console.log('Connecting to MongoDB...');
 mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
+
+        // Start the server
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}: http://localhost:${PORT}/`);
         });
     })
-    .catch(err => {
+    .catch((err) => {
         console.error('Error connecting to MongoDB', err.message);
     });
